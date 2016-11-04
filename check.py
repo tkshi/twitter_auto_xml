466ede500d1da0a563e4c0876b174f5f3ea2ae31
endo <takashi.endo@cluez.jp>
2016-11-04 19:09:00 +0900

    TwitterListと、GmailListの実装完了

---
+++ check.py
@@ -0,0 +1,9 @@
+arrayList = [
+    ['username','password','email@adress.com'],
+    ['username','password','email@adress.com']
+	]
+
+for row in arrayList:
+	print(row)
+	row[0] = 'anoanoano'
+print(arrayList)
