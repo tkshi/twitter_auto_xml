a9e5595518265030be5db2b00ea0c70b92439cf2
endo <takashi.endo@cluez.jp>
2016-11-08 14:49:57 +0900

    errorの時、エクセルの行を赤くする

--- Excel.py
+++ Excel.py
@@ -1,8 +1,8 @@
 import xlwt
 rows = [
-['a','b','c'],
-['a1','b1','c1'],
-['a2','b2','c2']
+['a','b','c','error'],
+['a1','b1','c1',],
+['a2','b2','c2','error']
 ]
 
 
@@ -26,11 +26,32 @@ def getSheet(fileName="test.xlsx",sheetName="Accounts"):
 
 	return result
 
-def writeSheet(fileName="sample.xlsx",sheetName="Accounts",rows=rows):
+def writeSheet(fileName="sample.xls",sheetName="Accounts",rows=rows):
 	book = xlwt.Workbook()
 	newSheet_1 = book.add_sheet(sheetName)
 
 	for row_i,row in enumerate(rows):
+		if 'error' in row:
+			xlwt.add_palette_colour("custom_colour", 0x21)
+			book.set_colour_RGB(0x21, 251, 228, 228)
+			style = xlwt.easyxf('pattern: pattern solid, fore_colour custom_colour')
+		else:
+			style = False
 		for i,cell in enumerate(row):
-			newSheet_1.write(row_i,i,cell)
+			if style == False:
+				newSheet_1.write(row_i,i,cell)
+			else:
+				newSheet_1.write(row_i,i,cell,style)
 	book.save(fileName)
+
+def setColor(sheetName="Accounts",rows=rows):
+	book = xlwt.Workbook()
+	sheet1 = book.add_sheet(sheetName)
+	xlwt.add_palette_colour("custom_colour", 0x21)
+	book.set_colour_RGB(0x21, 251, 228, 228)
+	style = xlwt.easyxf('pattern: pattern solid, fore_colour custom_colour')
+	sheet1.write(0, 0, 'Some text', style)
+	book.save('out.xls')
+
+if __name__ == '__main__':
+	writeSheet()
